0363a21e3cab86d97491ba92e9a8705d330df61e
b-chase <bcrawfc@gmail.com>
2020-10-09 13:30:09 -0400

    Update bayes mcmc.R

--- bayes mcmc.R
+++ bayes mcmc.R
@@ -1,7 +1,7 @@
 require(tidyverse)
 library(coda)
 
-n <- 500
+n <- 1000
 x <- runif(n, -10, 30)
 z <- runif(n, -30, -12)
 b1 <- 27
@@ -28,10 +28,10 @@ metro_mc <- function(data, iter = 10000, burn = 500, starting=NULL, proposals="r
   var.names <- c(names(data[,2:k]), "intercept", "nv")
   # k+1 terms if we count nv from t-distributed error (can also use precision or somethign else)
   
-  if (is.null(starting)) {starting <- c(rep(0,k), 5)}
+  if (is.null(starting)) {starting <- c(rep(0,k), 0.0001)}
   names(starting) <- var.names
   if (is.character(proposals)) {
-    proposals <- function(old.props) {new <- c(old.props[1:k] + rnorm(k,-10,10), rgamma(1, 1, 10))}
+    proposals <- function(old.props) {new <- c(old.props[1:k] + rnorm(k,-10,10), rgamma(1, 0.01, 1))}
   }
   if (is.character(priors)) {
     priors <- function(params) {c(rep(1,k), 1/params[k+1])}
@@ -41,12 +41,12 @@ metro_mc <- function(data, iter = 10000, burn = 500, starting=NULL, proposals="r
   
   likelihood <- function(inputs, params) {
     k <- length(params) - 1
-    nv <- params[k+1]
+    nv <- 1/sqrt(params[k+1])
     deviates <- apply(inputs, 1, function(pnt) 
       {pnt[1] - params[1:k] %*% pnt[2:(k+1)]})
     #print(summary(deviates))
     #print(nv)
-    LLH <- sum(log(dt(deviates, nv)))
+    LLH <- sum(log(dnorm(deviates, 0, nv)))
     LLH
   }
   
@@ -91,8 +91,11 @@ metro_mc <- function(data, iter = 10000, burn = 500, starting=NULL, proposals="r
     param.list <- rbind(param.list, new.row)
     #print("Full list:")
     #print(param.list)
-    if (i %% 50 == 0) {
-      cat(paste0("\r", "Number of Accepts / Proposals: ", accepts, " / ", i))
+    if (i %% 100 == 0) {
+      print(paste0("Number of Accepts / Proposals: ", accepts, " / ", i))
+      print("Last Accepted:")
+      print(new.row)
+      print(exp(alpha))
     }
   }
   cat("\nFinished\n")
@@ -100,7 +103,7 @@ metro_mc <- function(data, iter = 10000, burn = 500, starting=NULL, proposals="r
 }
 
 
-auto_mc <- function(data, batch.sizes=5000) {
+auto_mc <- function(data, batch.sizes=5000, total.batches=5) {
   k <- ncol(data) #number of variables, not including intercept
   print("First running a test chain of 2000 samples (burning first 500):")
   test.chain <- metro_mc(data, iter=2000, burn=500)
@@ -110,11 +113,12 @@ auto_mc <- function(data, batch.sizes=5000) {
   test.means <- apply(test.chain, 2, mean)
   test.var <- sapply(test.means, function(t) max(abs(t)/1.6, 1))
   if (test.accepts >= 3) {
-    test.var <- apply(test.chain, 2, function(m) {10*(max(m)-min(m))/sqrt(3)})
+    test.var <- apply(test.chain, 2, function(m) {(max(m)-min(m))/sqrt(3)})
     # i know this is messy, but using ifelse wasn't working, will try fixing it later
   }
-  print(test.means)
-  print(test.var)
+  print(as_tibble(rbind(test.means, test.var)))
+  Sys.sleep(5)
+  
   
   zone_in <- function(data.tib, last.chain, means, varis, batches=5, batch.size=5000, batch.burn=500) {
     k <- ncol(data.tib)
@@ -122,8 +126,8 @@ auto_mc <- function(data, batch.sizes=5000) {
       return(last.chain)
     } else {
       cat(paste0("\nBatches remaining: ", batches, "\n"))
-      chain.proposals <- function(old.params) {c(rnorm(k,means[1:k],2*varis[1:k]), 
-                                                 rgamma(1,means[k+1]^2/varis[k+1],varis[k+1]/means[k+1]))}
+      chain.proposals <- function(old.params) {c(old.params[1:k] + rnorm(k,0,sqrt(varis[1:k])), 
+                                                 rgamma(1,(means[k+1]^2)/varis[k+1],varis[k+1]/means[k+1]))}
       
       new.chain <- metro_mc(data, iter=batch.size, burn=batch.burn, proposals=chain.proposals, starting=means)
       
@@ -131,20 +135,21 @@ auto_mc <- function(data, batch.sizes=5000) {
       chain.means <- apply(new.chain, 2, mean)
       chain.varis <- sapply(chain.means, function(t) max(abs(t)/2, 1))
       if (chain.accepts >= 3) {
-        chain.varis <- apply(new.chain, 2, function(m) {5*(max(m)-min(m))/sqrt(3)})
+        chain.varis <- apply(new.chain, 2, function(m) {(max(m)-min(m))/sqrt(3)})
         # i know this is messy, but using ifelse() wasn't working, will try fixing it later
       }
-      print(rbind(chain.means, chain.varis))
+      print(as_tibble(rbind(chain.means, chain.varis)))
+      Sys.sleep(5)
       return(do.call(zone_in, args=list(data.tib, new.chain, chain.means, chain.varis, batches-1, batch.size, batch.burn)))
     }
   }
   
-  last.chain <- zone_in(data, test.chain, test.means, test.var, batch.size = batch.sizes, batch.burn = 0)
+  last.chain <- zone_in(data, test.chain, test.means, test.var, batches=total.batches, batch.size=batch.sizes, batch.burn=0)
   par(mfcol=c(2,ceiling(k/2)+1))
   traceplot(mcmc(last.chain))  
   
   last.chain
 }
 
-my.chain <- auto_mc(data)
+my.chain <- auto_mc(data, batch.sizes = 5000, total.batches = 5)
 summary(my.chain)
